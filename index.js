ae4956c04b4b850281f623799e4d80ca21859d41
Mohammed Rizwan <MohammedRizwan6477@gmail.com>
2023-01-28 23:56:33 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-                                        // Array and loops
+// Array and loops
 
 // const languages = ['JavaScript','python','c','React.js','Angular'];
 
@@ -24,51 +24,50 @@
 
 // =====================================================================================================
 
-                                            // Loops
+// Loops
 
-    // for(i=0;i<4;i++){
-    //     console.log("*");
-    // }
+// for(i=0;i<4;i++){
+//     console.log("*");
+// }
 
+// const actors = [
+//     {
+//         name:'Actor 1',
+//         payment:100
+//     },
+//     {
+//         name:'Actor 2',
+//         payment:150
+//     },
+//     {
+//         name:'Actor 3',
+//         payment:200
+//     },
+// ]
 
-    // const actors = [
-    //     {
-    //         name:'Actor 1',
-    //         payment:100
-    //     },
-    //     {
-    //         name:'Actor 2',
-    //         payment:150
-    //     },
-    //     {
-    //         name:'Actor 3',
-    //         payment:200
-    //     },
-    // ]
+// <--- For loop --->
+// for(i=0;i<actors.length;i++){
+//     actors[i].payment = actors[i].payment-10
+// }
+// console.log(actors);
 
-    // <--- For loop --->
-    // for(i=0;i<actors.length;i++){
-    //     actors[i].payment = actors[i].payment-10
-    // }
-    // console.log(actors);
+// <--- ForEach --->
+// actors.forEach((actor)=>{
+//     actor.payment = actor.payment-10
+// })
 
-    // <--- ForEach --->
-    // actors.forEach((actor)=>{
-    //     actor.payment = actor.payment-10
-    // })
+// console.log(actors);
 
-    // console.log(actors);
+// <--- For of Loop --->
 
-    // <--- For of Loop --->
+// for(let actor of actors){
+//     actor.payment = actor.payment-20
+// }
 
-    // for(let actor of actors){
-    //     actor.payment = actor.payment-20
-    // }
-
-    // console.log(actors);
+// console.log(actors);
 
 // ============================================================================================
-    // <--- Filter --->
+// <--- Filter --->
 
 // const students = [
 //     {
@@ -90,7 +89,7 @@
 //         return true
 //     }
 //         return false;
-    
+
 // })
 
 // console.log(failed);
@@ -110,10 +109,10 @@
 //     },
 // ];
 
-// const finalUsers= users.map((user)=>{ 
+// const finalUsers= users.map((user,i)=>{
 //     return {
 //         // fullname:user.fname + " " + user.lname  old method
-//         fullname: `${user.fname} ${user.lname}`  
+//         fullname: `${user.fname} ${user.lname}`
 //     }
 // })
 
@@ -123,20 +122,20 @@
 
 // <--- Reduce --->
 
-const movies = [
-    {
-        name:'Interstellar',
-        budget:100
-    },
-    {
-        name:'Social',
-        budget:150
-    },
-    {
-        name:'Pathan',
-        budget:300
-    }
-];
+// const movies = [
+//   {
+//     name: "Interstellar",
+//     budget: 100,
+//   },
+//   {
+//     name: "Social",
+//     budget: 150,
+//   },
+//   {
+//     name: "Pathan",
+//     budget: 300,
+//   },
+// ];
 
 // let total = 0
 
@@ -146,10 +145,73 @@ const movies = [
 // })
 // console.log(total);
 
+// const total = movies.reduce((acc, curr) => {
+//   acc = acc + curr.budget;
+//   return acc;
+// }, 0);
 
-const total = movies.reduce((acc,curr)=>{
-    acc = acc+curr.budget;
-    return acc;
-},0)
+// console.log(total);
 
-console.log(total);
+// ====================================================================================================
+
+
+// <--- IndexOf --->
+
+// const admins = [2,1,5];
+
+// const user = {
+//     name: 'XYZ',
+//     id:5
+// }
+// const isAdmin = admins.indexOf(user.id) > -1
+// console.log(isAdmin);
+
+// <--- Includes --->
+
+// console.log(admins.includes(user.id));
+
+// ========================================================================================================
+
+// <--- Find --->
+
+// const users = [
+//     {
+//         name: 'XYZ',
+//         id:1
+//     },
+//     {
+//         name: 'abc',
+//         id:2
+//     },
+//     {
+//         name: 'pqr',
+//         id:3
+//     },
+// ];
+
+// const myUser = users.find((user)=>{
+//     if(user.id === 3){
+//         return true;
+//     }
+//     return false;
+// })
+
+// console.log(myUser);
+
+// ======================================================================================================
+
+// <--- Sort --->
+
+// const names = ['John','Jane','Shyam','Ram','Sameer','Mansi'];
+
+// names.sort()
+// console.log(names);
+
+// =========================================================================================================
+
+// <--- Spice --->
+
+// const names = ['John','Jane','Shyam','Ram','Sameer','Mansi'];
+
+// names.splice(2,1)
+// console.log(names);
